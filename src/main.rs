3e51bebe1c1dddee7a3ad863803a83f62bfd8ba4
Thejas B <thejas@hotstar.com>
2018-06-30 22:29:46 +0530

    Simple guessing game
    1. Made use of external crate `rand`

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Please enter your name");
-    let mut name = String::new();
+    println!("Number guessing game");
+    println!("Enter a number between 1 - 10");
+    let mut guessed_number = String::new();
+    let lucky_number : u32 = rand::thread_rng().gen_range(1, 10);
+
     io::stdin()
-        .read_line(&mut name)
+        .read_line(&mut guessed_number)
         .expect("Failed to read the name");
 
-    println!("The name is {}", name);
+    let number: u32 = guessed_number.trim().parse().expect("Not a number");
+
+    if lucky_number == number  {
+        println!("You guessed the right number {}", number)
+    } else {
+        println!("Sorry, the lucky number is {}", lucky_number)
+    }
 }
